2a4e6ca345402a5d9d4059338e81f4dfa78e2684
renanlucaz <renanluquinhas53@gmail.com>
2020-11-08 22:41:05 -0300

    feature: Criação de estrutura e página home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Image} from 'react-native'
+
+import { Container, Button, ButtonText, Title, Description, Icon } from './styles';
+
+import HomeIcon from '../../assets/home.png';
+import Arrow from '../../assets/vector/arrow.png';
+
+const Home = ({ navigation }) => {
+    function handleNavigate() {
+        navigation.navigate('Login');
+    }
+
+    return (
+        <Container>
+            <Icon source={HomeIcon} />
+            <Title>
+                Olá!
+            </Title>
+            <Description>
+                Nossas boas vindas a você, amante de filmes. Com o MyMovii você 
+                poderá descobrir novos filmes e adiciona-los a sua lista de 
+                favoritos.
+            </Description>
+            <Button onPress={handleNavigate}>
+                <ButtonText>Começar</ButtonText>
+                <Image source={Arrow} />
+            </Button>
+        </Container>
+    );
+}
+
+export default Home;
